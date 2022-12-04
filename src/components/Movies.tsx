c2827bda04be627bfa8f0cf15d31a244cebb13a0
Aiden Flynn <aidenflynn@MacBook-Pro.cable.rcn.com>
2022-12-04 17:37:54 -0500

    init no env

---
+++ src/components/Movies.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
+interface IMovie {
+    name: string,
+    genre: string,
+    year: number
+}
+
+const Movies = () => {
+    const [movies, setMovies] = useState<IMovie[]>([])
+
+    useEffect(() => {
+        // axios.get("https://still-mountain-45465.herokuapp.com/api/movies")
+        axios.get("http://localhost:3000/api/movies")
+        .then(resp => {
+          console.log(resp)
+          setMovies(resp.data)
+        })
+        .catch(err =>{
+          console.log(err)
+        })
+    }, [])
+
+    return (
+        <div>
+            {
+                movies && movies.map((movie, i) =>
+                    <ul key={i}>
+                    <li>{movie.name}</li>
+                    <li>{movie.genre}</li>
+                    <li>{movie.year}</li>
+                    </ul>
+                )
+            }
+        </div>
+    )
+}
+
+export default Movies;
